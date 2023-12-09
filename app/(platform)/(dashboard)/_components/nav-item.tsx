b66e819605bd55b7ed200fe4c49ec862559d1488
Tom Tarpey <tomtarpeydev@gmail.com>
2023-12-09 17:04:45 +0000

    add loading skeleton for sidebar

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -11,6 +11,7 @@ import {
 } from "lucide-react";
 import { useRouter, usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
     id : string;
@@ -106,3 +107,14 @@ export const NavItem = ({
         </AccordionItem>
     );
 }
+
+NavItem.Skeleton = function SkeletonNavItem() {
+    return (
+      <div className="flex items-center gap-x-2">
+        <div className="w-10 h-10 relative shrink-0">
+          <Skeleton className="h-full w-full absolute" />
+        </div>
+        <Skeleton className="h-10 w-full" />
+      </div>
+    );
+  };
